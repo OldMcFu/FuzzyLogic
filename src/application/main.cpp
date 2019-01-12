c738903966e548e58313c9baec797e1da797f338
OldMcFu <darkst0rm@web.de>
2019-01-12 23:19:34 +0100

    Add doxygen file

---
+++ src/application/main.cpp
@@ -0,0 +1,31 @@
+/** @mainpage FuzzyLogic: A project for the fuzzy c-means algortihm 
+*   @par Description:
+*   This project provide the functions for fuzzy c-means clustering.
+*
+*   @par Next paragraph - more information:
+*   TBD!
+*/
+
+// $Id$
+/**
+ * @file main.cpp
+ * The main file for the FuzzyLogic project.
+ *
+ * @brief Can use "brief" tag to explicitly generate comments for file documentation.
+ *
+ * @author OldMcFu 
+   [Note: depricated practice as CM systems like Subversion will maintain author info and blame logs.]
+ * @version 1 
+   [Note: depricated practice as CM systems like Subversion will maintain file history and revision numbers.]
+ */
+// $Log$
+
+#include <Eigen/Dense>
+
+
+
+
+int main (void)
+{
+    return 0;   //never reached!
+}
